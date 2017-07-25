9d774d53714397fa6c7c9748e50410ffaa562ea7
William Kim <esotericcoder@williams-mbp.attlocal.net>
2017-07-24 23:10:46 -0700

    Adding routes for create, and pyton app running functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,12 @@
 class MainController < ApplicationController
   def index
   end
+
+  def create
+    data = system("python #{Dir.pwd}/tutorialEdit/tutorial/spiders/quotes_spider.py")
+    respond_to do |format|
+      format.html
+      format.json { render json: params }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'main/index'
 
   root 'main#index'
+
+  post 'main/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
